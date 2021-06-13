f0d0c5b1c02e4b6f34617738b912904ec4d40240
Sid <sidreds06@gmail.com>
2021-06-13 12:01:57 +0530

    todo

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
@@ -60,6 +60,11 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.get("/", (req, res) => {
+  res.redirect('/todo');
+})
+
 app.get("/todo", (req, res) => {
   if(!req.session.list){
     req.session.list =[]
